725ce3f9946526005b8ae4ff31b1b772e0c273a5
Carlos Toledo <carlombo@hotmail.com>
2024-06-21 19:32:12 -0400

    [ADD] Consulta SQL en bruto

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.db import connection
 
 # Create your views here.
+def query1(request):
+    cursor = connection.cursor()
+    cursor.execute("insert into main_curso (cod, nombre, activo) values ('RP4', 'Repostería', true)")
+    return HttpResponse('Listo')
