a8f14395a4969d85df18818baba215f8ba25b0fc
Carlos Toledo <carlombo@hotmail.com>
2024-06-21 22:46:06 -0400

    [ADD] Se agrega modelos Cliente y Producto con llave foranea

--- dia2/models.py
+++ dia2/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    usuario = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = models.EmailField(max_length=50)
+
+class Producto(models.Model):
+    descripcion = models.CharField(max_length=500)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
