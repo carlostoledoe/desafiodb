95585e421b669d5a09ef239f4235a20fdc3218e0
Carlos Toledo <carlombo@hotmail.com>
2024-06-24 14:32:27 -0400

    [CHORE] Se agrega comentario

--- main/views.py
+++ main/views.py
@@ -3,6 +3,6 @@ from django.db import connection
 
 # Create your views here.
 def query1(request):
-    cursor = connection.cursor()
+    cursor = connection.cursor() #  Este cursor actúa como un “puntero” que nos permite interactuar con los resultados de una consulta a una base de datos
     cursor.execute("insert into main_curso (cod, nombre, activo) values ('RP4', 'Repostería', true)")
     return HttpResponse('Listo')
